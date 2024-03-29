e53916269b3e460a76f63701cf060da5002caef0
Ivan Filipenkov <hello@vanutp.dev>
2024-03-29 15:21:52 +0100

    gpg

--- dull-vessel/system/security.nix
+++ dull-vessel/system/security.nix
@@ -1,8 +1,14 @@
-{...}: {
+{pkgs, ...}: {
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
   };
 
   services.pcscd.enable = true;
+
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-curses;
+    enableSSHSupport = true;
+  };
 }
