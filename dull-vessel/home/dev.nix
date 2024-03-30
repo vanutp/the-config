d4f03d8a097eeb5396a747ffd3007f34092b9c4d
Ivan Filipenkov <hello@vanutp.dev>
2024-03-30 19:12:35 +0100

    fix scaling in intellij ides

--- dull-vessel/home/dev.nix
+++ dull-vessel/home/dev.nix
@@ -10,4 +10,19 @@
     userName = "Ivan Filipenkov";
     userEmail = "hello@vanutp.dev";
   };
+
+  
+  home.file = builtins.listToAttrs (
+    map
+    (vmoptionsPath: {
+      name = ".config/JetBrains/${vmoptionsPath}";
+      value = {
+        text = ''
+          -Xmx4096m
+          -Dsun.java2d.uiScale=1
+        '';
+      };
+    })
+    ["IntelliJIdea2023.3/idea64.vmoptions" "CLion2023.3/clion64.vmoptions"]
+  );
 }
