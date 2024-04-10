5ac15057d3b87ef1bb1298c413bc0be2b0b8e187
Ivan Filipenkov <hello@vanutp.dev>
2024-04-10 21:14:17 +0200

    reformat

--- dull-vessel/home/dev.nix
+++ dull-vessel/home/dev.nix
@@ -1,17 +1,19 @@
 {pkgs, ...}: {
-  home.file = builtins.listToAttrs (
-    map
-    (vmoptionsPath: {
-      name = ".config/JetBrains/${vmoptionsPath}";
-      value = {
-        text = ''
-          -Xmx4096m
-          -Dsun.java2d.uiScale=1
-        '';
-      };
-    })
-    ["IntelliJIdea2023.3/idea64.vmoptions" "CLion2023.3/clion64.vmoptions"]
-  ) // {
-    ".local/share/jdks/temurin8".source = pkgs.temurin-bin-8;
-  };
+  home.file =
+    builtins.listToAttrs (
+      map
+      (vmoptionsPath: {
+        name = ".config/JetBrains/${vmoptionsPath}";
+        value = {
+          text = ''
+            -Xmx4096m
+            -Dsun.java2d.uiScale=1
+          '';
+        };
+      })
+      ["IntelliJIdea2023.3/idea64.vmoptions" "CLion2023.3/clion64.vmoptions"]
+    )
+    // {
+      ".local/share/jdks/temurin8".source = pkgs.temurin-bin-8;
+    };
 }
