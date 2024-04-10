ac7b171f74ac94df0112796e00762ea7c912704b
Ivan Filipenkov <hello@vanutp.dev>
2024-04-10 14:10:53 +0200

    p1: fix pg_hba

--- servers/p1/system/postgresql.nix
+++ servers/p1/system/postgresql.nix
@@ -3,9 +3,9 @@
     enable = true;
     enableTCPIP = true;
     authentication = ''
-      # TYPE  DATABASE  USER  ADDRESS       METHOD
-      # podman networks (actually podman uses only 10.89.0.0-10.255.255.255, but i don't want to clutter the config)
-      host    all       all   10.128.0.0/9  scram-sha-256
+      # TYPE  DATABASE  USER  ADDRESS     METHOD
+      # podman + wg networks
+      host    all       all   10.0.0.0/8  scram-sha-256
     '';
   };
 }
