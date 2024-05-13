5a3e53b21d618a5562d773a8e1f3ffc94e677d5e
Ivan Filipenkov <hello@vanutp.dev>
2024-05-13 06:49:37 +0200

    remove useless

--- common/fox/shell/unclutter.nix
+++ common/fox/shell/unclutter.nix
@@ -6,7 +6,6 @@
   home.sessionVariables = {
     WAKATIME_HOME = "${config.xdg.configHome}/wakatime";
     PYTHONSTARTUP = "${config.xdg.configHome}/python/pythonrc";
-    _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${config.xdg.configHome}/java";
     NUGET_PACKAGES = "${config.xdg.cacheHome}/NuGetPackages";
     NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/npmrc";
     NODE_REPL_HISTORY = "${config.xdg.dataHome}/node_repl_history";
