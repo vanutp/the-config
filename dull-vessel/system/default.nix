c441dd4e43ca034017c1e425a3a062fbe55e9f60
Ivan Filipenkov <hello@vanutp.dev>
2024-06-05 20:23:08 +0200

    add kyocera drivers, move all hardware-related stuff to a separate file

--- dull-vessel/system/default.nix
+++ dull-vessel/system/default.nix
@@ -16,6 +16,7 @@
     ./audio.nix
     ./portals.nix
     ./login.nix
+    ./hardware.nix
   ];
 
   boot.loader.systemd-boot.enable = true;
@@ -36,44 +37,11 @@
   systemd.services.NetworkManager-wait-online.enable = false;
   users.extraGroups.networkmanager.members = ["fox"];
 
-  services.printing.enable = true;
-
   programs.dconf.enable = true;
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-    settings = {
-      General = {
-        FastConnectable = true;
-      };
-    };
-  };
-  services.blueman.enable = true;
-
   services.gpm.enable = true;
-  hardware.opengl.enable = true;
-
-  services.udev.packages = with pkgs; [
-    via
-  ];
-
-  services = {
-    upower.enable = true;
-    power-profiles-daemon.enable = true;
-  };
-
   programs.gamemode.enable = true;
-
   services.flatpak.enable = true;
-
   services.gvfs.enable = true;
   boot.supportedFilesystems = ["ntfs"];
-
-  services.fprintd.enable = true;
-
-  services.fwupd.enable = true;
-
   virtualisation.waydroid.enable = true;
-
-  hardware.opentabletdriver.enable = true;
 }
---
+++ dull-vessel/system/hardware.nix
@@ -0,0 +1,28 @@
+{pkgs, ...}: {
+  hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      settings = {
+        General = {
+          FastConnectable = true;
+        };
+      };
+    };
+    opengl.enable = true;
+    opentabletdriver.enable = true;
+  };
+
+  services = {
+    printing = {
+      enable = true;
+      drivers = [pkgs.cups-kyodialog];
+    };
+    blueman.enable = true;
+    udev.packages = [pkgs.via];
+    upower.enable = true;
+    power-profiles-daemon.enable = true;
+    fprintd.enable = true;
+    fwupd.enable = true;
+  };
+}
