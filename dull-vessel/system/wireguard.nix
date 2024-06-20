2b9bc6af71415651b4aecfd357362c58ad84c54a
Ivan Filipenkov <hello@vanutp.dev>
2024-06-20 23:46:30 +0200

    add wg2-only

--- dull-vessel/system/wireguard.nix
+++ dull-vessel/system/wireguard.nix
@@ -22,7 +22,20 @@
         {
           publicKey = "67Br5DrR+rGYPojECxyV2CnTxgAtUzhDdE6WAjaXpAI=";
           allowedIPs = ["0.0.0.0/0"];
-          endpoint = "217.151.231.127:51820";
+          endpoint = "89.110.74.194:51820";
+          persistentKeepalive = 30;
+        }
+      ];
+    };
+    wg2-only = {
+      autostart = true;
+      address = ["10.3.1.2/16"];
+      privateKeyFile = config.sops.secrets."wg_keys/wg2".path;
+      peers = [
+        {
+          publicKey = "67Br5DrR+rGYPojECxyV2CnTxgAtUzhDdE6WAjaXpAI=";
+          allowedIPs = ["10.3.0.0/16"];
+          endpoint = "89.110.74.194:51820";
           persistentKeepalive = 30;
         }
       ];
