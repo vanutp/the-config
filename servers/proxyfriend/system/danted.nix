8c1cd7a93f67c9d873d416af26606e37b13a16ae
Ivan Filipenkov <hello@vanutp.dev>
2024-06-20 23:46:19 +0200

    proxyfriend: add wg1 and ipv6 to dante

--- servers/proxyfriend/system/danted.nix
+++ servers/proxyfriend/system/danted.nix
@@ -13,7 +13,9 @@
     enable = true;
     config = ''
       internal: 10.1.0.5 port = 1080
+      internal: 10.3.0.1 port = 1080
       external: 89.110.74.194
+      external: 2a14:1e00:1:122::
       clientmethod: none
       socksmethod: username
       client pass {
@@ -21,10 +23,10 @@
         log: connect error
       }
       socks pass {
-        from: 0.0.0.0/0 to: 0.0.0.0/0
+        from: 0/0 to: 0/0
       }
     '';
   };
-  systemd.services.dante.requires = ["wg-quick-wg0.service"];
+  systemd.services.dante.requires = ["wg-quick-wg0.service" "wg-quick-wg1.service"];
   networking.firewall.allowedTCPPorts = [1080];
 }
