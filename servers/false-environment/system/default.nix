5fe6527f8585c20134a132807cbda6efa0a49177
Ivan Filipenkov <hello@vanutp.dev>
2024-07-05 04:12:05 +0200

    lumious!

--- servers/false-environment/system/default.nix
+++ servers/false-environment/system/default.nix
@@ -32,13 +32,51 @@
     temurin-bin-17
     rdiff-backup
   ];
-  networking.firewall.allowedTCPPorts = [25565];
-  networking.firewall.allowedUDPPorts = [24454];
+  networking.firewall.allowedTCPPorts = [80 443 25565];
+  networking.firewall.allowedUDPPorts = [443 24454];
 
   services.cron = {
     enable = true;
     systemCronJobs = [
       "0 * * * * fox rdiff-backup ~/server/world/ ~/backup/"
+    ];
+  };
+
+  services.nginx = {
+    enable = true;
+    validateConfigFile = false;
+    virtualHosts."mc.rightarion.ru" = {
+      addSSL = true;
+      enableACME = true;
+      locations."/" = {
+        root = "/var/www/mc.rightarion.ru/modpack";
+        extraConfig = "autoindex on;";
+      };
+      locations."/launcher" = {
+        alias = "/var/www/mc.rightarion.ru/launcher/";
+        extraConfig = "autoindex on;";
+      };
+      locations."= /win".return = "302 /launcher/lumious_launcher.exe";
+      locations."= /linux".return = "302 /launcher/lumious_launcher_linux";
+      locations."= /macos".return = "302 /launcher/lumious_launcher_macos.dmg";
+    };
+    virtualHosts."map.rightarion.ru" = {
+      addSSL = true;
+      enableACME = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8100";
+      };
+    };
+  };
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "hello@vanutp.dev";
+  };
+  users.users.build = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      common.constants.pubkeys.main
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCZ1H5VPM2JfkdR2NQvqnUdcwkosNpRSbP5l9sgL0RNcJj+CiWC6iygi7Ra3o84nUtIaTg/d2nofpK1dEiXkQoliPofdkJTxorRnAOYdegaAGT54N4R0KaYzsRJsPqMO+AsCTEz4BV2D6HEuyz8/Ht8aBsCctLCL+YeEjYF+mH2Rlz7NOuAvii9RnPsnpBDGGBAk25pPNW9qN3A97S85JrWlkScIxNhLpTfKU1uKioSHJjHd70MbJRdYHrSjwRbrrzHhOyza6OCezC0y0yeObqHoIT9uhRJ/bSeG1CTidZ+5/jc+OHVMQCfhQvsVfwnJnlv1g7iK/qtEmeoa4T4pv/iv3AqaoI9DgBvX7jJBg4fb7Gsmgod70aY5xW5ioPti0tAcUySLvOad+Cn1/sxRV4bnxY4waU/tTNQKxP0e4oGlS0Ypg9APbDFgP5r+Fk8ReWYoEynATTejevLbHIVQXSbydf1+78rleY1Iu5gPoU6bLH9GAl0TPYe/Bqr0mNAauE= dva-smp@s1"
     ];
   };
 }
