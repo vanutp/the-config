a4cefedf902914e861b30156a3bb2eb1e646295b
Ivan Filipenkov <hello@vanutp.dev>
2024-09-09 05:47:33 +0200

    oops

--- servers/embassy/system/containers/vanutp_music_bot.nix
+++ servers/embassy/system/containers/vanutp_music_bot.nix
@@ -2,7 +2,6 @@
   virtualisation.composter.apps.vanutp_music_bot.services.main = {
     image = "ghcr.io/vanutp/spotify-bot:latest";
     environment = {
-      PROXY = "socks5://s1-proxy:UiHMXAUessu4AG@10.1.0.5:1080/";
       SPOTIFY_REDIRECT_URI = "http://localhost:8088";
       OWNER_ID = "304493639";
     };
