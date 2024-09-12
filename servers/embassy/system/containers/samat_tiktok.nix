c83d3fa4fe5fc8adcdeda67b5f7addd54a050b48
Ivan Filipenkov <hello@vanutp.dev>
2024-09-13 00:47:55 +0200

    change samat_tiktok

--- servers/embassy/system/containers/samat_tiktok.nix
+++ servers/embassy/system/containers/samat_tiktok.nix
@@ -8,7 +8,7 @@
         REDIS_SET_KEY = "tiktok_posted";
         REDIS_URL = "redis://redis";
         TELEGRAM_CHANNEL_ID = "-1001501810341";
-        TELEGRAM_OWNER_ID = "550578377";
+        TELEGRAM_OWNER_ID = "304493639";
         TIKTOK_CHECK_PERIOD_MAX = "300000";
         TIKTOK_CHECK_PERIOD_MIN = "180000";
         TIKTOK_SEC_UID = "MS4wLjABAAAAA7FNRArU9pUvQaUyQf6wm157BACKar4KtXhMaotHuvLRgSkPYt-hNi6tYwo2lhoe";
