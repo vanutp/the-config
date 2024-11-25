346036eca1fca252029beea7c1a7ee74d4c597ef
Ivan Filipenkov <hello@vanutp.dev>
2024-11-25 14:46:54 +0100

    combined bluetooth audio sink

--- dull-vessel/system/audio.nix
+++ dull-vessel/system/audio.nix
@@ -6,6 +6,38 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    extraConfig.pipewire."bt-combined" = {
+      "context.modules" = [
+        {
+          name = "libpipewire-module-combine-stream";
+          args = {
+            "combine.mode" = "sink";
+            "node.name" = "bt_broadcast";
+            "node.description" = "A combined sink to all bluetooth devices";
+            "combine.latency-compensate" = false;
+            "combine.props" = {
+              "audio.position" = ["FL" "FR"];
+            };
+            "stream.props" = {};
+            "stream.rules" = [
+              {
+                matches = [
+                  {
+                    "node.name" = "~bluez_output.*";
+                    "media.class" = "Audio/Sink";
+                  }
+                ];
+                actions = {
+                  create-stream = {
+                  };
+                };
+              }
+            ];
+          };
+        }
+      ];
+    };
   };
 
   environment.systemPackages = with pkgs; [
