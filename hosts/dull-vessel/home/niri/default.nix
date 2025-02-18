400d2428485a3bec2e32ca78b9a85a4092dbaa71
Ivan Filipenkov <hello@vanutp.dev>
2025-02-18 10:21:59 +0100

    always open new nautilus window

--- hosts/dull-vessel/home/niri/default.nix
+++ hosts/dull-vessel/home/niri/default.nix
@@ -156,7 +156,7 @@ in {
               "Super+Escape" = spawn ["loginctl" "lock-session"];
               "Super+Shift+A" = spawn yubikey-totp;
               "Super+D" = spawn ["tofi-drun" "--drun-launch=true"];
-              "Super+E" = spawn "nautilus";
+              "Super+E" = spawn ["nautilus" "-w"];
               "Ctrl+Shift+D" = spawn ["copyq" "toggle"];
               "Super+S" = "screenshot";
 
