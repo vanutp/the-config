cb3000255fc988cfbf78f40083c72bde0486975a
Ivan Filipenkov <hello@vanutp.dev>
2025-03-11 18:50:32 +0100

    +mergiraf

--- modules/git/home.nix
+++ modules/git/home.nix
@@ -1,12 +1,24 @@
 {
+  pkgs,
+  pkgs-unstable,
   config,
   lib,
   ...
 }: {
+  home.packages = [pkgs-unstable.mergiraf];
   programs.git = lib.mkIf (config.home.username == "fox") {
     enable = true;
     lfs.enable = true;
     userName = "Ivan Filipenkov";
     userEmail = "hello@vanutp.dev";
+    extraConfig = {
+      core.attributesfile = builtins.toString (pkgs.runCommand "gitattributes" {} ''
+        ${lib.getExe pkgs-unstable.mergiraf} languages --gitattributes > $out
+      '');
+      "merge \"mergiraf\"" = {
+        name = "mergiraf";
+        driver = "mergiraf merge --git %O %A %B -s %S -x %X -y %Y -p %P -l %L";
+      };
+    };
   };
 }
