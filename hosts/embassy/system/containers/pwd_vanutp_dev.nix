5f866adf59a91ae06eaeaa5d32e878ca24d09129
Ivan Filipenkov <hello@vanutp.dev>
2025-05-02 22:37:00 +0200

    update vaultwarden config for latest version

--- hosts/embassy/system/containers/pwd_vanutp_dev.nix
+++ hosts/embassy/system/containers/pwd_vanutp_dev.nix
@@ -3,19 +3,9 @@
     backup.enable = true;
     services.vaultwarden = {
       image = "vaultwarden/server:latest";
-      traefik = {
-        host = "pwd.vanutp.dev";
-        port = 80;
-      };
-      labels = {
-        "traefik.http.routers.pwd__vanutp__dev.service" = "pwd__vanutp__dev";
-        "traefik.http.routers.pwd__vanutp__dev__ws.rule" = "Host(`pwd.vanutp.dev`) && Path(`/notifications/hub`)";
-        "traefik.http.routers.pwd__vanutp__dev__ws.service" = "pwd__vanutp__dev__ws";
-        "traefik.http.services.pwd__vanutp__dev__ws.loadbalancer.server.port" = "3012";
-      };
+      traefik.host = "pwd.vanutp.dev";
       environment = {
         DATABASE_MAX_CONNS = "2";
-        WEBSOCKET_ENABLED = "true";
       };
       env_file = "secrets.env";
       volumes = ["./data:/data"];
