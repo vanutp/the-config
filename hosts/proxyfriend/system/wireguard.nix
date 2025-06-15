676e87f273a427c71f2e25c4c3f9ccbea8ffb0e4
Ivan Filipenkov <hello@vanutp.dev>
2025-06-15 18:31:02 +0200

    proxyfriend: amneziawg

--- hosts/proxyfriend/system/wireguard.nix
+++ hosts/proxyfriend/system/wireguard.nix
@@ -5,7 +5,7 @@
   ...
 }: {
   networking.nat.enable = true;
-  networking.firewall.allowedUDPPorts = [51820];
+  networking.firewall.allowedUDPPorts = [51820 45372];
   networking.wg-quick.interfaces = let
     makeWg0 = import "${self}/utils/makeWg0.nix";
   in {
@@ -16,8 +16,20 @@
     };
     wg1 = {
       address = ["10.3.0.1/16"];
-      listenPort = 51820;
+      listenPort = 45372;
       privateKeyFile = config.sops.secrets."wg_keys/wg1".path;
+      type = "amneziawg";
+      extraOptions = {
+        Jc = 12;
+        Jmin = 567;
+        Jmax = 696;
+        S1 = 82;
+        S2 = 44;
+        H1 = 913185510;
+        H2 = 561456501;
+        H3 = 1095959564;
+        H4 = 448867668;
+      };
 
       # TODO: do this using networking.nat
       postUp = ''
@@ -31,39 +43,14 @@
 
       peers = [
         {
-          # s2
-          publicKey = "R50v8ZmGFPPjoHXNfAPEnv7c9B7Hcs7Nq6DYQ9Q/kg8=";
-          allowedIPs = ["10.3.0.2/32"];
-        }
-        {
-          # laptop
-          publicKey = "0HcEd9CrX9+MgCfx51KvYVL/C6aHGHIzcwGk2k4iX04=";
+          # monoblock
+          publicKey = "eRDI0NIsMGRRAbXsl4xID23fIFSybIzbei4iGXHPNlY=";
           allowedIPs = ["10.3.1.2/32"];
         }
         {
-          # phone
-          publicKey = "fupQFVCXLXt9wY5v0TPoS28FpZi+QsnZp1RdrzU5+Ec=";
-          allowedIPs = ["10.3.1.4/32"];
-        }
-        {
-          # filusha
-          publicKey = "ymFzfMJN7+pufjwlIE/NFX28FWavQ56LRWHytw/n1kQ=";
-          allowedIPs = ["10.3.1.5/32"];
-        }
-        {
-          # elena
-          publicKey = "dOK2UXSpa0ds/odkPDwPmYiXH2RvZORu/nmLNDdDghE=";
-          allowedIPs = ["10.3.1.6/32"];
-        }
-        {
           # gravity
-          publicKey = "P8XqFnDJOs6zzEVAy4CL5PAYuLfYencxUlDlRg3FGkg=";
-          allowedIPs = ["10.3.1.7/32"];
-        }
-        {
-          # lumi
-          publicKey = "q+AywhwRwZBdtd8IlEBQIzr4kVtBZa3NyAnEPJ9QPSw=";
-          allowedIPs = ["10.3.1.8/32"];
+          publicKey = "UsokN3qGp3Us30Grg/VraZsSc6KZN+1+sEJRwxYZD2Y=";
+          allowedIPs = ["10.3.1.3/32"];
         }
       ];
     };
