d592c13480f62e5c39a1b4c7533da62a082ca8c2
Ivan Filipenkov <hello@vanutp.dev>
2025-09-05 17:42:38 +0200

    update 64gram

--- packages/_64gram/package.nix
+++ packages/_64gram/package.nix
@@ -1,12 +1,12 @@
 pkgs: let
   pname = "64gram";
-  version = "1.1.77";
+  version = "1.1.78";
   binary = pkgs.stdenv.mkDerivation {
     pname = "${pname}-bin";
     inherit version;
     src = pkgs.fetchzip {
       url = "https://github.com/TDesktop-x64/tdesktop/releases/download/v${version}/64Gram_${version}_linux.zip";
-      hash = "sha256-klAPRiOzThblAv64sqGtN0OqhrXdfDxO0CaEEIqQ70I=";
+      hash = "sha256-2ksgpl0eZhFB/br6ZO7oCN/FcCd5a+ufHFf6FIRn054=";
       stripRoot = false;
     };
     buildPhase = ''
