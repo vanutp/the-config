faebd2a7b4207375c6ef6ba7df36121a71e6cb2d
Ivan Filipenkov <hello@vanutp.dev>
2025-10-09 01:44:49 +0200

    memory-hole: add restic alias for archives

--- hosts/memory-hole/system/default.nix
+++ hosts/memory-hole/system/default.nix
@@ -1,4 +1,8 @@
-{...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   imports = [
     ./containers
     ./disko.nix
@@ -19,4 +23,18 @@
   systemd.network.wait-online.enable = false;
 
   networking.hostId = "1dc14032";
+
+  environment.systemPackages = [
+    (pkgs.writeShellScriptBin "restic-archives" ''
+      set -euo pipefail
+      if [[ $(id -u) -ne 0 ]]; then
+        echo "This script must be ran as root" >&2
+        exit 1
+      fi
+      export $(cat ${config.sops.secrets."restic/archives/repo-creds".path} | xargs)
+      export RESTIC_PASSWORD_FILE=${config.sops.secrets."restic/archives/password".path}
+      export RESTIC_REPOSITORY=s3:https://s3.eu-central-003.backblazeb2.com/vanutp-archives
+      exec restic $@
+    '')
+  ];
 }
