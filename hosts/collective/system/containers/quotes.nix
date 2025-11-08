928fd89583e895ca684434ac99004be8bbc56eab
Ivan Filipenkov <hello@vanutp.dev>
2025-11-08 16:06:34 +0100

    fix quotes

--- hosts/collective/system/containers/quotes.nix
+++ hosts/collective/system/containers/quotes.nix
@@ -1,15 +1,18 @@
 {config, ...}: {
-  virtualisation.composter.apps.quotes.services = {
-    bot = {
-      image = "registry.vanutp.dev/vanutp/quote-bot:latest";
-      environment.QUOTE_API_URL = "http://api:3000";
-      env_file = config.sops.secrets."quotes/bot".path;
-      volumes = ["./data:/app/data"];
-    };
-    api = {
-      image = "registry.vanutp.dev/vanutp/quote-api:latest";
-      env_file = config.sops.secrets."quotes/api".path;
-      traefik.host = "quotes.vanutp.dev";
+  virtualisation.composter.apps.quotes = {
+    backup.enable = true;
+    services = {
+      bot = {
+        image = "registry.vanutp.dev/vanutp/quote-bot:latest";
+        environment.QUOTE_API_URL = "http://api:3000";
+        env_file = config.sops.secrets."quotes/bot".path;
+        volumes = ["./data:/app/data"];
+      };
+      api = {
+        image = "registry.vanutp.dev/vanutp/quote-api:latest";
+        env_file = config.sops.secrets."quotes/api".path;
+        traefik.host = "quotes.vanutp.dev";
+      };
     };
   };
 }
