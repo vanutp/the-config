02146f65dac9c4bf4e94176dcf373a8f2843374d
Ivan Filipenkov <hello@vanutp.dev>
2025-11-27 02:46:46 +0100

    move mvn.vtp.sh to embassy and change domain to maven.vanutp.dev

---
+++ hosts/embassy/system/containers/maven_vanutp_dev.nix
@@ -0,0 +1,25 @@
+{...}: {
+  virtualisation.composter.apps.maven_vanutp_dev.services = {
+    main = {
+      image = "ghcr.io/dzikoysk/reposilite";
+      traefik = {
+        host = "maven.vanutp.dev";
+        proxied = false;
+      };
+      volumes = [
+        "./data:/app/data"
+      ];
+    };
+  };
+  vanutp.traefik.extraDynamicConfig = {
+    http.routers.mvn_vtp_sh = {
+      rule = "Host(`mvn.vtp.sh`)";
+      middlewares = ["mvn_vtp_sh"];
+      service = "noop@internal";
+    };
+    http.middlewares.mvn_vtp_sh.redirectregex = {
+      regex = "^https://mvn\\.vtp\\.sh/(.*)";
+      replacement = "https://maven.vanutp.dev/\${1}";
+    };
+  };
+}
