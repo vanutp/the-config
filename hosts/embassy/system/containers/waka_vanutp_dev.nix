6d03c092e15067c294b8615a860f9d5793d97ae8
Ivan Filipenkov <hello@vanutp.dev>
2025-11-28 23:25:28 +0100

    wakapi: update, add sso, remove mail

--- hosts/embassy/system/containers/waka_vanutp_dev.nix
+++ hosts/embassy/system/containers/waka_vanutp_dev.nix
@@ -1,19 +1,27 @@
-{...}: {
+{config, ...}: {
   virtualisation.composter.apps.waka_vanutp_dev.services.wakapi = {
-    # TODO: update shit
-    image = "registry.vanutp.dev/vanutp/wakapi";
+    # waiting for https://github.com/muety/wakapi/pull/879
+    image = "registry.vanutp.dev/vanutp/wakapi:latest";
     traefik = {
       host = "waka.vanutp.dev";
-      port = 3000;
+      proxied = false;
     };
+    env_file = config.sops.secrets.wakapi.path;
     environment = {
-      WAKAPI_ALLOW_SIGNUP = "false";
       WAKAPI_DB_TYPE = "postgres";
-      WAKAPI_DB_MAX_CONNECTIONS = "1";
-      WAKAPI_MAIL_ENABLED = "true";
-      WAKAPI_MAIL_PROVIDER = "smtp";
-      WAKAPI_VIBRANT_COLOR = "true";
+      WAKAPI_DB_HOST = "10.1.0.4";
+      WAKAPI_DB_PORT = "5432";
+      WAKAPI_DB_USER = "wakapi";
+      WAKAPI_DB_NAME = "wakapi";
+
+      WAKAPI_INSECURE_COOKIES = "false";
+      WAKAPI_ALLOW_SIGNUP = "false";
+      WAKAPI_PUBLIC_URL = "https://waka.vanutp.dev";
+
+      WAKAPI_OIDC_ALLOW_SIGNUP = "true";
+      WAKAPI_OIDC_PROVIDERS_0_NAME = "authentik";
+      WAKAPI_OIDC_PROVIDERS_0_DISPLAY_NAME = "vanutp one";
+      WAKAPI_OIDC_PROVIDERS_0_ENDPOINT = "https://one.vanutp.dev/application/o/wakapi/";
     };
-    env_file = "secrets.env";
   };
 }
