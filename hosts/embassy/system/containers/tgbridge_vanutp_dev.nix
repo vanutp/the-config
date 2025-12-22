c1a9178a8790cf7ac202bb692763af797a3620a0
Ivan Filipenkov <hello@vanutp.dev>
2025-12-22 20:20:05 +0100

    remove cf for tgbridge

--- hosts/embassy/system/containers/tgbridge_vanutp_dev.nix
+++ hosts/embassy/system/containers/tgbridge_vanutp_dev.nix
@@ -1,6 +1,9 @@
 {...}: {
   virtualisation.composter.apps.tgbridge_vanutp_dev.services.main = {
     image = "ghcr.io/vanutp/tgbridge:latest";
-    traefik.host = "tgbridge.vanutp.dev";
+    traefik = {
+      host = "tgbridge.vanutp.dev";
+      proxied = false;
+    };
   };
 }
